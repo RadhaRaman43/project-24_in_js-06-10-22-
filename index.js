3e956836895e408de1d66043e6d38c9ac65fc918
RadhaRaman43 <ramanradha0434@gmail.com>
2022-10-04 15:34:43 +0530

    this is first java rpogram

---
+++ index.js
@@ -0,0 +1,24 @@
+let n=20
+let c=3
+let a=10
+let b=n+a
+let d=n/a
+let m=n*a
+let M=n%c
+let lo=n<a 
+
+console.log(a, c, a, b, d, m, M, lo)
+
+var t="radha"
+var g="raman"
+var g=t+g
+console.log(g)
+console.log(m>M && m<M)
+console.log(n>c || n<c)
+
+const bb=m|b
+const tb=d && b
+console.log(bb,tb)
+
+
+
