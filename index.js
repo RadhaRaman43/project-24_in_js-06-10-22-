e9d9f94f69b79edc7492452f503b7a6a91ae87e4
RadhaRaman43 <ramanradha0434@gmail.com>
2022-10-09 09:41:03 +0530

    this is Object Methos

--- index.js
+++ index.js
@@ -1,41 +1,94 @@
-/*var myBalance=0;
-var msg="";
-while(myBalance<=10){
-    msg=msg+"My balance is "+ myBalance +"<br>";
-    myBalance++;
-    
-}
-document.write(msg);
-"<br>"
-var m='';
-do
+/*let course={Book:"JavaScript",BookId:10,BookPrice:400,
+books:["Java","C","C++"],
+Address:{                                               //nested method
+    company:"Google",
+    Country:"India"
+},
+showbooks()
 {
-    m=m+"Mybalance is "+myBalance+"<br>";
-    myBalance++;
+    let msg=this.Book
+    let s=this.books.forEach(function(value){
+        console.log(value)
+    })
+    console.log(msg)
+}
+}
+course.showbooks()*/
+/*
+let obj={sen:{sen1:"Javascript is a object-based language. Learning Nested Data Structures."},
+showSentence(){
+    let msg=this.sen.sen1
+    console.log(msg)
+}
+}
+obj.showSentence()*/
+
+
+
+/*let array={
+    key1:["title: Ramayan", "author: Valmiki", "bookId: 24"],
+    //key2:["title: Mahabharat","author: Vedvyas","bookId: 25" ],
+    //key3:["title: Geeta","author: Krishna","bookId: 26"]
+    showData(){
+    let msg=this.key1.forEach(function(value,index){
+        console.log(index,value)
+    })
+    }
+ }
+ array.showData()*/
+
+ 
+ let codeLan={
+              lan:["Python","Java","Ruby","Go"],
+              key1:"languagea",key2:"is challanging",key3:"isRewarding",key4:"difficulty",
+              key5:"isFun",key6:true,key7:7,key8:true,
+showValue()
+{
+    for(i=0;i<this.lan.length;i++){
+        let msg=this.lan[i]
+        console.log(msg)
+    }
+    console.log(this.key1)
+    console.log(this.key2)
+    console.log(this.key3)
+    console.log(this.key4)
+    console.log(this.key5)
+    console.log(this.lan)
+    console.log(this.key6)
+    console.log(this.key7)
+    console.log(this.key8)
 
 }
-while(myBalance<=30)
-document.write(m);
-
-for(var a=10;a<15;a++){
-    console.log(a);
-}*/
-
-
-
-var count=0;
-var marks=0;
-while(count<5){
-    var number=prompt("enter the your marks");
-    marks+=parseInt(number);
-    count++;
 }
-document.write(marks)
+codeLan.showValue()
 
-/*var total=0;
-for(var i=0;i<5;i++){
-    var no=prompt("Enter the marks");
-    total+=parseInt(no);
 
+
+/*var a=prompt()
+var sum=0
+while(a>0){
+    var r=a%10
+    sum+=r
+    a=a/10
 }
-document.write(total)*/
+
+console.log(parseInt(sum))*/
+
+/*let obj=[{k1:"Kamal", key2:"Seeta"},
+         {key3:"Hello",key4:"Hi"},
+        {key6:"Ice",key7:"cream"} ]
+
+let arr=obj.map(function(item){
+    return item;
+})
+console.log(arr)*/
+
+
+
+
+
+
+
+
+
+
